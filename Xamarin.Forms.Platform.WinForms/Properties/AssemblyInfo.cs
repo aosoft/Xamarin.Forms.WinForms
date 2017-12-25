da1b563b8273199d22adfd9ac81122903d3fb53e
aosoft <tan-y@aosoft.jp>
2017-12-25 13:53:27 +0900

    add EditorRenderer, EntryRenderer

--- Xamarin.Forms.Platform.WinForms/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Platform.WinForms/Properties/AssemblyInfo.cs
@@ -45,3 +45,5 @@ using Xamarin.Forms.Platform.WinForms;
 [assembly: ExportRenderer(typeof(Button), typeof(ButtonRenderer))]
 [assembly: ExportRenderer(typeof(Slider), typeof(SliderRenderer))]
 [assembly: ExportRenderer(typeof(Switch), typeof(SwitchRenderer))]
+[assembly: ExportRenderer(typeof(Editor), typeof(EditorRenderer))]
+[assembly: ExportRenderer(typeof(Entry), typeof(EntryRenderer))]
---
+++ Xamarin.Forms.Platform.WinForms/Renderers/EntryRenderer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Xamarin.Forms.Platform.WinForms
+{
+	public class EntryRenderer : ViewRenderer<Entry, System.Windows.Forms.TextBox>
+	{
+		/*-----------------------------------------------------------------*/
+		#region Event Handler
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				base.OnElementChanged(e);
+
+				if (e.NewElement != null)
+				{
+					Control.Multiline = true;
+
+					UpdateText();
+					UpdateTextColor();
+					UpdateFont();
+				}
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == Editor.TextProperty.PropertyName)
+			{
+				UpdateText();
+			}
+			else if (e.PropertyName == Editor.TextColorProperty.PropertyName)
+			{
+				UpdateTextColor();
+			}
+			else if (e.PropertyName == Editor.FontSizeProperty.PropertyName ||
+				e.PropertyName == Editor.FontAttributesProperty.PropertyName)
+			{
+				UpdateFont();
+			}
+
+			base.OnElementPropertyChanged(sender, e);
+		}
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Internals
+
+		void UpdateText()
+		{
+			var nativeElement = Control;
+			if (nativeElement != null)
+			{
+				nativeElement.Text = Element.Text;
+			}
+		}
+
+		void UpdateTextColor()
+		{
+			var nativeElement = Control;
+			if (nativeElement != null)
+			{
+				var color = Element.TextColor;
+				nativeElement.ForeColor =
+					color == Color.Default ?
+						System.Drawing.SystemColors.ControlText :
+						color.ToWindowsColor();
+			}
+		}
+
+		void UpdateFont()
+		{
+			var nativeElement = Control;
+			var element = Element;
+			if (nativeElement != null)
+			{
+				nativeElement.Font = new System.Drawing.Font(
+					element.FontFamily,
+					(float)element.FontSize,
+					element.FontAttributes.ToWindowsFontStyle());
+			}
+		}
+
+		#endregion
+	}
+}
