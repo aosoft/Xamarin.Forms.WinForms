5a9a4c4a5f8f9cccdeeb1dd7c72848b9b6305751
aosoft <tan-y@aosoft.jp>
2018-01-04 19:03:44 +0900

    Added LayoutRenderer.OnElementChanged

--- Xamarin.Forms.Platform.WinForms/Renderers/LayoutRenderer.cs
+++ Xamarin.Forms.Platform.WinForms/Renderers/LayoutRenderer.cs
@@ -3,8 +3,22 @@ namespace Xamarin.Forms.Platform.WinForms
 {
 	public class LayoutRenderer : ViewRenderer<Layout, System.Windows.Forms.Panel>
 	{
-		public LayoutRenderer()
+		/*-----------------------------------------------------------------*/
+		#region Event Handler
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Layout> e)
 		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				if (Control == null)
+				{
+					SetNativeControl(new System.Windows.Forms.Panel());
+				}
+			}
 		}
+
+		#endregion
 	}
 }
